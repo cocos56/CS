aed22a888d80636050e39a19eb5b3fbbf2ca57cb
coco <zj175@139.com>
2019-12-26 02:03:14 +0800

    更新于2019年12月26日	 2:03:13.20

--- C Sharp课堂授课代码2019/第4单元 面向对象编程进阶2/15Event事件/Program.cs
+++ C Sharp课堂授课代码2019/第4单元 面向对象编程进阶2/15Event事件/Program.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace _15Event事件
 {
 	class Program
@@ -13,14 +8,14 @@ namespace _15Event事件
 			Friend friend1 = new Friend("张三");
 			Friend friend2 = new Friend("李四");
 			Friend friend3 = new Friend("王五");
-			//订阅事件
+
+			//订阅事件，指明事件触发后由谁处理
 			bridegroom.MarryEvent +=
 				new Bridegroom.MarryHandler(friend1.SendMessage);
 			bridegroom.MarryEvent += 
 				new Bridegroom.MarryHandler(friend2.SendMessage);
 			//触发事件 事件的调用只能在定义事件的内部调用
 			bridegroom.onMarriageComing("朋友们，结婚了");
-
 		}
 	}
 }
